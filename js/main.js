1e3a68ffc7a183dd837d019db53aee62a98c140e
Gordon L <70592442+itsgordo@users.noreply.github.com>
2022-07-01 01:12:11 -0400

    project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // ===== hamburger menu =====
+  var $hamburger = $(".hamburger");
+  $hamburger.on("click", function (e) {
+    $hamburger.toggleClass("is-active");
+    $("#nav").toggleClass("active");
+  });
 
-    var $hamburger = $(".hamburger");
-      $hamburger.on("click", function(e) {
-        $hamburger.toggleClass("is-active");
-        $('#nav').toggleClass('active');
-      });
-    
+  // ===== duck generator =====
+
+  // generate image
+  var click = false;
+  $("#click-duck").click(function () {
+    getapi("https://random-duck.api.stdlib.com/api@1.2.1/quack/gif/");
+    $("#random-duck").show();
+    click = true;
+  });
+
+  // hide/show button
+  var degrees = 0;
+  $("#close").click(function () {
+    degrees += 180;
+    $(this).css({
+      transform: "rotate(" + degrees + "deg)",
     });
+    if (click == true) {
+      $("#click-duck").toggle();
+      $("#random-duck").toggle();
+    } else {
+      $("#click-duck").toggle();
+    }
+  });
+});
+
+async function getapi(url) {
+  // Storing response
+  const response = await fetch(url);
+  // Storing data in form of JSON
+  var data = await response.json();
+  // Copies data into array
+  document.getElementById("random-duck").src = data.url;
+}
